2abeb5821408356f4a77a6a16ec8b6d613c2ad1d
Чужегин Андрей А. <achuzhegin@u-energo.ru>
2025-10-20 22:39:02 +0400

    cecond commit

--- web/index.php
+++ web/index.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,16 +10,21 @@
   <link rel="stylesheet" href="style.css" />
 </head>
 <?php
-session_start();
+$host  = $_SERVER['HTTP_HOST'];
+$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+print_r("$host$uri");
+// echo "$host$uri/$extra";
 if (isset($_SESSION["login"]) && trim($_SESSION["login"]) !== "") {
   echo "<div class= 'exit'>";
   echo "<p>Добрый день " . $_SESSION["login"] . "</p>";
-  echo "<a href='http://localhost:8000/web/exit.php'>Выйти</a>";
+  echo "<a href='http://$host$uri/exit.php'>Выйти</a>";
   echo "</div>";
 } else {
-  include("web/authForm.html");
+  include("authForm.html");
 }
 // echo "<pre>";
 // var_dump($_SESSION);
 // var_dump(trim($_SESSION["login"]));
-// echo ("</pre>");
+// echo "</pre>";
+
+
--- web/post.php
+++ web/post.php
@@ -1,8 +1,14 @@
-
 <?php
 session_start();
-if (isset($_POST["login"]) && trim($_POST["login"])!=="") {
-  $_SESSION["login"] = trim($_POST["login"]);
+$host  = $_SERVER['HTTP_HOST'];
+$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+// echo "https://$host$uri/index.php";
+if (isset($_POST['login']) && trim($_POST['login']) !== '') {
+  $_SESSION['login'] = trim($_POST['login']);
 }
-header("Location: http://localhost:8000" . "/web/index.php");
+header("Location: http://$host$uri/index.php");
 exit;
+// echo "<pre>";
+// var_dump($_SESSION);
+// var_dump(trim($_SESSION["login"]));
+// echo "</pre>";
